b09ecd18d1007123d182bb654afe7b7a327e9bde
Het0c <hlopez.htilg@gmail.com>
2024-10-15 00:33:16 -0300

    Error-404 v1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,21 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'not-found',
+    loadChildren: () => import('./not-found/not-found.module').then( m => m.NotFoundPageModule)
+  },
+
+  {
+    path: 'not-found', //Error 404
+    loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundPageModule)
+  },
+  {
+    path: '**', //al ingresar a un dominio no verificado, lanzara al instante a not-found
+    redirectTo: 'not-found'
   }
+
 ];
 @NgModule({
   imports: [
