21f987721f87962237b60a04110db6729bea97b4
Gonzalo <gon.gallardo@duocuc.cl>
2024-12-08 21:08:34 -0300

    Notificaciones push

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleBooksService } from '../services/book.service';
 import { NavController } from '@ionic/angular';
+import { ActionPerformed, PushNotificationSchema, PushNotifications, Token } from '@capacitor/push-notifications';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-tab1',
@@ -39,7 +41,43 @@ export class Tab1Page implements OnInit {
 
   constructor(private googleBooksService: GoogleBooksService, private navCtrl: NavController) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('Initializing HomePage');
+
+      if (Capacitor.isNativePlatform()) {
+      // Request permission to use push notifications
+      // iOS will prompt user and return if they granted permission or not
+      // Android will just grant without prompting
+      PushNotifications.requestPermissions().then((result) => {
+        if (result.receive === 'granted') {
+          // Register with Apple / Google to receive push via APNS/FCM
+          PushNotifications.register();
+        } else {
+          // Show some error
+        }
+      });
+  
+      // On success, we should be able to receive notifications
+      PushNotifications.addListener('registration', (token: Token) => {
+        alert('Push registration success, token: ' + token.value);
+      });
+  
+      // Some issue with our setup and push will not work
+      PushNotifications.addListener('registrationError', (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      });
+  
+      // Show us the notification payload if the app is open on our device
+      PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      });
+  
+      // Method called when tapping on a notification
+      PushNotifications.addListener('pushNotificationActionPerformed', (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      });
+    }
+  }
   onSearchChange(event: any) {
     const query = this.searchQuery.toLowerCase();
     if (query) {
@@ -85,53 +123,4 @@ export class Tab1Page implements OnInit {
   }
 }
 
-/*
-@Component({
-  selector: 'app-home',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
-})
-
-
-  
-  export class HomePage implements OnInit {
-    ngOnInit() {
-      console.log('Initializing HomePage');
-
-      if (Capacitor.isNativePlatform()) {
-      // Request permission to use push notifications
-      // iOS will prompt user and return if they granted permission or not
-      // Android will just grant without prompting
-      PushNotifications.requestPermissions().then((result) => {
-        if (result.receive === 'granted') {
-          // Register with Apple / Google to receive push via APNS/FCM
-          PushNotifications.register();
-        } else {
-          // Show some error
-        }
-      });
-  
-      // On success, we should be able to receive notifications
-      PushNotifications.addListener('registration', (token: Token) => {
-        alert('Push registration success, token: ' + token.value);
-      });
-  
-      // Some issue with our setup and push will not work
-      PushNotifications.addListener('registrationError', (error: any) => {
-        alert('Error on registration: ' + JSON.stringify(error));
-      });
-  
-      // Show us the notification payload if the app is open on our device
-      PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
-        alert('Push received: ' + JSON.stringify(notification));
-      });
-  
-      // Method called when tapping on a notification
-      PushNotifications.addListener('pushNotificationActionPerformed', (notification: ActionPerformed) => {
-        alert('Push action performed: ' + JSON.stringify(notification));
-      });
-    }
-  }
-  }
-
-*/
+ 
